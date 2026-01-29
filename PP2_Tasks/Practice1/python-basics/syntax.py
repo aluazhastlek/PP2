cabf4edb5f39b9d7010d3e2ac171d1d3399eeb8d
ZhastilekAlua <a_zhastilek@kbtu.kz>
2026-01-29 13:38:55 +0500

    prac01

---
+++ PP2_Tasks/Practice1/python-basics/syntax.py
@@ -0,0 +1,14 @@
+#1
+if 5 > 2:
+    print("Five is greater than two!")
+
+#2
+if 5 > 2:
+print("Five is greater than two!")
+
+#3
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
